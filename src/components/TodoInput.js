e98efc5219bce1a1ab4e467cf24e1ee359953211
oberaij <ijobera@springvalley.tech>
2018-03-08 14:28:57 +0800

    Simple React Todos - Source Codes added

---
+++ src/components/TodoInput.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import './TodoInput.css';
+
+class TodoInput extends Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            value: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.addTodo = this.addTodo.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+           value: e.target.value
+        });
+    }
+
+    addTodo(todo){
+        // Ensure the todo text isn't empty
+        if (todo.length > 0) {
+            this.props.addTodo(todo);
+            this.setState({
+               value: ''
+            });
+        }
+    }
+
+    render() {
+        return (
+          <div>
+              <input type="text" placeholder="Please enter your todos..." value={this.state.value} onChange={this.handleChange}/>
+              <button className="btn btn-primary" onClick={() => this.addTodo(this.state.value)}>Submit</button>
+          </div>
+        );
+    }
+}
+
+export default TodoInput;
